37116925c2fb1fd3aad12b3791ba0b1bd71fe8b4
Datta-Landage <dattalandge001@gmail.com>
2023-12-15 21:08:43 +0530

    fix: api

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ const { Route } = require("express");
 const cors = require("cors");
 const app = express();
 
+const pdfModel = require("./Models/pdfModel");
+
 // Enable All CORS Requests for development use
 app.use(cors());
+
+app.get("/mypdfData", async (req, res) => {
+  const pdfData = await pdfModel.find({ isDeleted: false });
+  res.status(200).send({
+    status: true,
+    msg: "pdfData retrieved succesfully",
+    data: pdfData,
+  });
+});
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
