5f4b932a6e536fdb6e66db3ffc6cb84ecafe70e6
Nikita Lilhore <102968844+Lnikita123@users.noreply.github.com>
2024-02-06 15:24:30 +0530

    Update usercontactController.js

--- src/Controllers/usercontactController.js
+++ src/Controllers/usercontactController.js
@@ -8,8 +8,8 @@ const userContactData = async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "nikitalilhore123@gmail.com",
-        pass: "dzjfxzvmwndjwmme",
+        user:"stmichaels866@gmail.com",
+        pass:"btqjqursctqvgcbz",
       },
     });
     const emailAddresses = [
@@ -19,7 +19,7 @@ const userContactData = async (req, res) => {
     ];
     for (i = 0; i < emailAddresses.length; i++) {
       const mailOptions = {
-        from: "nikitalilhore123@gmail.com",
+         from:"stmichaels866@gmail.com",
         to:` ${emailAddresses[i]}`,
         subject: "New Student Details Submission",
         text: `
