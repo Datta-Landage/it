7205ca5a3364dd3edfe4fc1e49246a6774d607af
Nikita Lilhore <102968844+Lnikita123@users.noreply.github.com>
2024-02-13 11:05:10 +0530

    Update formpopupController.js

--- src/Controllers/formpopupController.js
+++ src/Controllers/formpopupController.js
@@ -21,8 +21,8 @@ const formPopuData = async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-          user:"nikitalilhore123@gmail.com",
-        pass:"dzjfxzvmwndjwmme",
+          user:"webleadsms@gmail.com",
+        pass:"uhpdmijbubtmqewz",
       },
     });
     const emailAddresses = [
@@ -32,7 +32,7 @@ const formPopuData = async (req, res) => {
     ];
     for (i = 0; i < emailAddresses.length; i++) {
       const mailOptions = {
-        from:"nikitalilhore123@gmail.com",
+        from:"webleadsms@gmail.com",
         to: `${emailAddresses[i]}`,
         subject: "New Student Details Submission",
         text: `
